1442a530a6fbaaae01ac74a269142c4e7ad66fc8
BryanFlynt <bryan.flynt@noaa.gov>
2017-12-15 16:48:15 +0000

    Added toConfig to oops::Varibales

--- src/oops/base/Variables.cc
+++ src/oops/base/Variables.cc
@@ -17,40 +17,45 @@
 
 // -----------------------------------------------------------------------------
 namespace oops {
-// -----------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------
 
-Variables::Variables(const eckit::Configuration & conf)
- : convention_(""), vars_(0)
-{
-  conf.get("variables", vars_);
-}
-
-// -----------------------------------------------------------------------------
-
-Variables::Variables(const std::vector<std::string> & vars, const std::string & conv)
- : convention_(conv), vars_(vars)
-{}
-
-// -----------------------------------------------------------------------------
-
-Variables::Variables(const Variables & other)
- : convention_(other.convention_), vars_(other.vars_)
-{}
-
-// -----------------------------------------------------------------------------
-
-Variables::~Variables() {}
-
-// -----------------------------------------------------------------------------
-
-void Variables::print(std::ostream & os) const {
-  for (size_t jj = 0; jj < vars_.size(); ++jj) {
-    if (jj > 0) os << ", ";
-    os << vars_[jj];
+  Variables::Variables(const eckit::Configuration & conf)
+    : convention_(""), vars_(0), conf_()
+  {
+    conf.get("variables", vars_);
+    conf_.set("nvars", vars_.size());
+    conf_.set("variables", vars_);
   }
-  if (!convention_.empty()) os << " (" << convention_ << ")";
-}
 
-// -----------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------
+
+  Variables::Variables(const std::vector<std::string> & vars, const std::string & conv)
+    : convention_(conv), vars_(vars), conf_()
+  {
+    conf_.set("nvars", vars_.size());
+    conf_.set("variables", vars_);
+  }
+
+  // -----------------------------------------------------------------------------
+
+  Variables::Variables(const Variables & other)
+    : convention_(other.convention_), vars_(other.vars_)
+  {}
+
+  // -----------------------------------------------------------------------------
+
+  Variables::~Variables() {}
+
+  // -----------------------------------------------------------------------------
+
+  void Variables::print(std::ostream & os) const {
+    for (size_t jj = 0; jj < vars_.size(); ++jj) {
+      if (jj > 0) os << ", ";
+      os << vars_[jj];
+    }
+    if (!convention_.empty()) os << " (" << convention_ << ")";
+  }
+
+  // -----------------------------------------------------------------------------
 
 }  // namespace oops
--- src/oops/base/Variables.h
+++ src/oops/base/Variables.h
@@ -12,37 +12,38 @@
 #include <string>
 #include <vector>
 
-#include "util/Printable.h"
+#include <boost/scoped_ptr.hpp>
 
-namespace eckit {
-  class Configuration;
-}
+#include "eckit/config/LocalConfiguration.h"
+#include "util/Printable.h"
 
 namespace oops {
 
-// -----------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------
 
-class Variables : public util::Printable {
- public:
-  static const std::string classname() {return "oops::Variables";}
+  class Variables : public util::Printable {
+  public:
+    static const std::string classname() {return "oops::Variables";}
 
-  explicit Variables(const eckit::Configuration &);
-  Variables(const std::vector<std::string> &, const std::string & conv = "");
+    explicit Variables(const eckit::Configuration &);
+    Variables(const std::vector<std::string> &, const std::string & conv = "");
 
-  ~Variables();
+    ~Variables();
 
-  Variables(const Variables &);
+    Variables(const Variables &);
 
-  const std::vector<std::string> & variables() const {return vars_;}
+    const std::vector<std::string> & variables() const {return vars_;}
+    const eckit::Configuration & asConfig() const {return conf_;}
 
- private:
-  void print(std::ostream &) const;
+  private:
+    void print(std::ostream &) const;
 
-  std::string convention_;
-  std::vector<std::string> vars_;
-};
+    std::string convention_;
+    std::vector<std::string> vars_;
+    eckit::LocalConfiguration conf_;
+  };
 
-// -----------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------
 
 }  // namespace oops
 
