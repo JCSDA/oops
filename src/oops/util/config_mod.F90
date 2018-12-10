6cf150473c8de129c465cb51aff77b6d64eaea50
hailingz <36716145+hailingz@users.noreply.github.com>
2018-12-10 11:39:09 -0700

    pass default value as optional if config_element_exists is false (#123)
    * pass default value as optional if config_element_exists is false
    
    * remove default for config_get_string_vector
    
    * remove default for config_get_string_vector
    
    * bug fix

--- src/oops/util/config_mod.F90
+++ src/oops/util/config_mod.F90
@@ -17,6 +17,7 @@ use kinds
 implicit none
 
 private
+integer, parameter :: max_string=800
 public config_element_exists, config_get_int, config_get_real, config_get_string, &
        config_get_string_vector
 
@@ -47,18 +48,28 @@ end function config_element_exists
 
 !>  Return data from a Config element as a integer.
 
-integer function config_get_int(c_dom,query)
+integer function config_get_int(c_dom,query,idefault)
   implicit none
   type(c_ptr), intent(in) :: c_dom
   character(len=*), intent(in) :: query
-
+  integer, intent(in), optional :: idefault
   character(kind=c_char,len=1), allocatable :: c_query(:)
+  character(max_string) :: err_msg
 
   !  Translate query from Fortran string to C++ char[].
   call f_c_string(query,c_query)
 
   ! Call C++ to process the query
-  config_get_int = c_config_get_data_as_int(c_dom,c_query)
+  if(LOGICAL(c_config_element_exists(c_dom,c_query))) then
+    config_get_int = c_config_get_data_as_int(c_dom,c_query)
+  else
+    if (present(idefault)) then
+      config_get_int = idefault
+    else
+      write(err_msg,*) "config_get_int: ", trim(query), " does not exist in config and no default"
+      call abor1_ftn(err_msg)
+    endif
+  endif
   deallocate(c_query)
 end function config_get_int
 
@@ -66,18 +77,28 @@ end function config_get_int
 
 !>  Return data from a Config element as a real.
 
-real(kind=kind_real) function config_get_real(c_dom,query)
+real(kind=kind_real) function config_get_real(c_dom,query,rdefault)
   implicit none
   type(c_ptr), intent(in) :: c_dom
   character(len=*), intent(in) :: query
-
+  real(kind_real), intent(in), optional :: rdefault
   character(kind=c_char,len=1), allocatable :: c_query(:)
+  character(max_string) :: err_msg
 
   !  Translate query from Fortran string to C++ char[].
   call f_c_string(query,c_query)
 
   ! Call C++ to process the query
-  config_get_real = c_config_get_data_as_double(c_dom,c_query)
+  if(LOGICAL(c_config_element_exists(c_dom,c_query))) then
+    config_get_real = c_config_get_data_as_double(c_dom,c_query)
+  else
+    if (present(rdefault)) then
+      config_get_real = rdefault
+    else
+      write(err_msg,*) "config_get_real: ", trim(query), " does not exist in config and no default"
+      call abor1_ftn(err_msg)
+    endif
+  endif
   deallocate(c_query)
 end function config_get_real
 
@@ -85,31 +106,40 @@ end function config_get_real
 
 !>  Return the contents of a Config element as a string
 
-function config_get_string(c_dom,length,query)
+function config_get_string(c_dom,length,query,sdefault)
   implicit none
   type(c_ptr), intent(in) :: c_dom
-  integer, intent(in) :: length
+  integer,     intent(in) :: length
   character(len=*), intent(in) :: query
-  character(len=length) ::  config_get_string
-
-  character(kind=c_char,len=1), allocatable :: c_query(:), c_data(:)
+  character(len=length)                       ::  config_get_string
+  character(len=*), intent(in), optional ::  sdefault
+  character(kind=c_char,len=1), allocatable   :: c_query(:), c_data(:)
   integer nchars
+  character(max_string) :: err_msg
 
   !  Translate query from Fortran string to C++ char[].
   call f_c_string(query,c_query)
 
   ! Call C++ to process the query
-  nchars = c_config_get_data_length(c_dom,c_query)
-  if (nchars > length) &
-    call abor1_ftn('config_get_string: return argument too short')
+  if(LOGICAL(c_config_element_exists(c_dom,c_query))) then
 
-  if (nchars>0) then
-    allocate(c_data(nchars+1))
-    call c_config_get_data(c_dom,c_query,c_data)
-    call c_f_string(c_data, config_get_string)
+     nchars = c_config_get_data_length(c_dom,c_query)
+     if (nchars > length) &
+       call abor1_ftn('config_get_string: return argument too short')
+
+     allocate(c_data(nchars+1))
+     call c_config_get_data(c_dom,c_query,c_data)
+     call c_f_string(c_data, config_get_string)
+
   else
-    call abor1_ftn('config_get_string: element not found')
-!    config_get_string = ""
+
+    if (present(sdefault)) then
+      config_get_string = sdefault
+    else
+      write(err_msg,*) "config_get_string: ", trim(query), " does not exist in config and no default"
+      call abor1_ftn(err_msg)
+    endif
+
   endif
 
   deallocate(c_query)
