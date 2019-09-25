5eea8e0080852716676167cb7e19482ad8f3fbc9
Daniel Holdaway <27729500+danholdaway@users.noreply.github.com>
2019-09-25 18:49:43 -0400

    Feature/changevartesting (#404)
    * allow for a variable change in the convert state application
    
    * add symmetry test for nonlinear variable change
    
    * use getBool with internal conditional for inverse
    
    * another if bites the dust

--- qg/mains/qgConvertState.cc
+++ qg/mains/qgConvertState.cc
@@ -1,17 +1,19 @@
 /*
- * (C) Copyright 2018 UCAR
+ * (C) Copyright 2018-2019 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+#include "model/instantiateQgChangeVarFactory.h"
 #include "model/QgTraits.h"
 #include "oops/runs/ConvertState.h"
 #include "oops/runs/Run.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
-  oops::ConvertState<qg::QgTraits> fc;
-  run.execute(fc);
+  qg::instantiateQgChangeVarFactory();
+  oops::ConvertState<qg::QgTraits> cs;
+  run.execute(cs);
   return 0;
 }
