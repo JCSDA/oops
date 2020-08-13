6ab35df8aad958a0cb97139c7e5dc4e51b2f19e9
Anna Shlyaeva <shlyaeva@gmail.com>
2020-08-13 14:50:57 -0700

    remove LocalHofX application, clean LocalObsSpace test (#798)

--- src/test/interface/LocalObsSpace.h
+++ src/test/interface/LocalObsSpace.h
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2017-2019 UCAR
+ * (C) Copyright 2017-2020 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -26,11 +26,11 @@
 namespace test {
 
 // -----------------------------------------------------------------------------
-
+/// Tests that number of local observations in LocalObsSpace is the same as in reference
 template <typename OBS> void testLocalObsSpace() {
-  typedef ObsTestsFixture<OBS> Test_;
-  typedef oops::ObsSpace<OBS>               LocalObsSpace_;
-  typedef oops::ObsVector<OBS>              ObsVector_;
+  typedef ObsTestsFixture<OBS>         Test_;
+  typedef oops::ObsSpace<OBS>          LocalObsSpace_;
+  typedef oops::ObsVector<OBS>         ObsVector_;
 
   const eckit::LocalConfiguration localconf(TestEnvironment::config(), "local obs space");
 
@@ -40,20 +40,19 @@ template <typename OBS> void testLocalObsSpace() {
   double lat = geolocconf.getDouble("lat");
   const eckit::geometry::Point2 center(lon, lat);
 
-  // get localization info from yaml
+  // get localization test from yaml
   eckit::LocalConfiguration locconf(localconf, "localization");
 
+  // count local nobs for all obs types
   int totalNobs = 0;
-
-  for (std::size_t jj = 0; jj < Test_::obspace().size(); ++jj) {
+  for (size_t jj = 0; jj < Test_::obspace().size(); ++jj) {
     // initialize local observation space
     LocalObsSpace_ localobs(Test_::obspace()[jj], center, locconf);
-    oops::Log::info() << "Local obs within " << locconf << " from " << center <<
+    oops::Log::test() << "Local obs within " << locconf << " from " << center <<
                          ": " << localobs << std::endl;
 
     // count local nobs
     ObsVector_ localvec(localobs);
-    ObsVector_ globvec(Test_::obspace()[jj]);
     totalNobs += localvec.nobs();
   }
 
@@ -63,41 +62,43 @@ template <typename OBS> void testLocalObsSpace() {
 }
 
 // -----------------------------------------------------------------------------
-
+/// Tests that constructing local ObsVector from local ObsSpace by reading
+/// (ObsVector(const ObsSpace &, const std::string &) ctor), and by subsetting
+/// full ObsVector (ObsVector(const ObsSpace &, const ObsVector &) ctor)
+/// give the same results
 template <typename OBS> void testLocalObsVector() {
-  typedef ObsTestsFixture<OBS> Test_;
+  typedef ObsTestsFixture<OBS>              Test_;
   typedef oops::ObsSpace<OBS>               LocalObsSpace_;
   typedef oops::ObsVector<OBS>              ObsVector_;
 
   const eckit::LocalConfiguration localconf(TestEnvironment::config(), "local obs space");
+  // get center (for localization) from yaml
+  eckit::LocalConfiguration geolocconf(localconf, "location");
+  double lon = geolocconf.getDouble("lon");
+  double lat = geolocconf.getDouble("lat");
+  const eckit::geometry::Point2 center(lon, lat);
 
-  for (std::size_t jj = 0; jj < Test_::obspace().size(); ++jj) {
-    // get center (for localization) from yaml
-    eckit::LocalConfiguration geolocconf(localconf, "location");
-    double lon = geolocconf.getDouble("lon");
-    double lat = geolocconf.getDouble("lat");
-    const eckit::geometry::Point2 center(lon, lat);
+  // get localization test from yaml
+  eckit::LocalConfiguration locconf(localconf, "localization");
+  const std::string varname = localconf.getString("variable name");
 
-    // get localization info from yaml
-    eckit::LocalConfiguration locconf(localconf, "localization");
-
-    const std::string varname = localconf.getString("variable name");
-
+  for (size_t jj = 0; jj < Test_::obspace().size(); ++jj) {
     // initialize full ObsVector for a specified variable
     ObsVector_ fullvec(Test_::obspace()[jj], varname);
-    oops::Log::info() << "Full Obsvector: " << fullvec << std::endl;
+    oops::Log::test() << "Full Obsvector: " << fullvec << std::endl;
 
     // initialize local observation space
     LocalObsSpace_ localobs(Test_::obspace()[jj], center, locconf);
-    oops::Log::info() << "Local obs within " << locconf << " from " << center <<
-                         ": " << localobs << std::endl;
+    oops::Log::test() << "Local obs within " << locconf << " from " << center <<
+                           ": " << localobs << std::endl;
 
     // intialize local obsvector by reading specified variable from local obsspace
     ObsVector_ localvec1(localobs, varname);
-    oops::Log::info() << "Local Obsvector from Local Obsspace: " << localvec1 << std::endl;
+    oops::Log::test() << "Local Obsvector from Local Obsspace: " << localvec1 << std::endl;
+
     // initialize local obsvector from full obsvector using local obsspace
     ObsVector_ localvec2(localobs, fullvec);
-    oops::Log::info() << "Local ObsVector from full ObsVector: " << localvec2 << std::endl;
+    oops::Log::test() << "Local ObsVector from full ObsVector: " << localvec2 << std::endl;
     // check that the two are equal
     EXPECT(localvec1.nobs() == localvec2.nobs());
     localvec2 -= localvec1;
