8e79a33b9e42cde1476ac718ed009dca827970b5
Chris Thomas <32307951+ctgh@users.noreply.github.com>
2020-09-23 14:13:25 +0100

    tests of Vector3D (#1007)
    Co-authored-by: Anna Shlyaeva <shlyaeva@gmail.com>

--- src/test/assimilation/PCG.h
+++ src/test/assimilation/PCG.h
@@ -25,6 +25,44 @@
 
 namespace test {
 
+  void test_Vector3D()
+  {
+    // Test each function in the Vector3D class
+
+    const Vector3D v1(1.0, 2.0, 3.0);
+    const Vector3D v2(4.0, 5.0, 6.0);
+
+    Vector3D vEq = v1;
+    EXPECT(vEq.x() == 1.0 && vEq.y() == 2.0 && vEq.z() == 3.0);
+
+    Vector3D vAdd = v1;
+    vAdd += v2;
+    EXPECT(vAdd.x() == 5.0 && vAdd.y() == 7.0 && vAdd.z() == 9.0);
+
+    Vector3D vSub = v2;
+    vSub -= v1;
+    EXPECT(vSub.x() == 3.0 && vSub.y() == 3.0 && vSub.z() == 3.0);
+
+    Vector3D vMult = v1;
+    vMult *= 2.0;
+    EXPECT(vMult.x() == 2.0 && vMult.y() == 4.0 && vMult.z() == 6.0);
+
+    Vector3D vMultV = v1;
+    vMultV *= v2;
+    EXPECT(vMultV.x() == 4.0 && vMultV.y() == 10.0 && vMultV.z() == 18.0);
+
+    Vector3D vDivV = v2;
+    vDivV /= v1;
+    EXPECT(vDivV.x() == 4.0 && vDivV.y() == 2.5 && vDivV.z() == 2.0);
+
+    Vector3D vAxpy = v1;
+    vAxpy.axpy(3, v2);
+    EXPECT(vAxpy.x() == 13.0 && vAxpy.y() == 17.0 && vAxpy.z() == 21.0);
+
+    const double dotprod = v1.dot_product_with(v2);
+    EXPECT(dotprod == 32.0);
+  }
+
   void test_PCG()
   {
     // Solve Ax = b using the PCG algorithm
@@ -71,6 +109,10 @@ namespace test {
     EXPECT(oops::is_close_absolute(x.y(), 3.0, 1.0e-9));
     EXPECT(oops::is_close_absolute(x.z(), 4.0, 1.0e-9));
     EXPECT(oops::is_close_absolute(normReduction, 0.0, 1.0e-9));
+  }
+
+  CASE("assimilation/PCG/Vector3D") {
+    test_Vector3D();
   }
 
   CASE("assimilation/PCG/PCG") {
