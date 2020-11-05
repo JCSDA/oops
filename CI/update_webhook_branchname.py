5a1c5cbdb05a85bda6c842dbca48eb5dc0de7234
Maryam Abdi <maryamao@ucar.edu>
2020-11-05 08:25:08 -0700

    switch to jcsda-internal (#958)
    * switch to jcsda-internal
    
    * update webhook_branchname
    
    * bugfix
    
    * bugfix

--- CI/update_webhook_branchname.py
+++ CI/update_webhook_branchname.py
@@ -7,9 +7,8 @@
 
 """
 Codebuild Runs this script to check for branches with
-same names accross downstream repos. Then creates a
+same names accross dependent repos. Then creates a
 webhook on PR page to notify users if such a branch exists.
-
 Call as:
 update_webhook_branchname.py branchName commitId
 """
@@ -24,30 +23,31 @@ commitId = sys.argv[2]
 
 
 # check other repos
-repoList = ["ufo", "saber", "ioda", "soca", "fv3-jedi", "mpas-jedi", "shallow-water"]
+repoList = ["atlas", "saber", "ioda", "ufo", "soca", "fv3-jedi", "mpas-jedi"]
 
 token = os.getenv('GIT_PASS', '...')
 g = Github(token)
-owner = "JCSDA"
+ownerList = ["JCSDA", "jcsda-internal"]
 
-oopsRepo = g.get_repo("jcsda/oops")
+oopsRepo = g.get_repo("jcsda-internal/oops")
 
-for repoName in repoList:
-  branchExists = True
-  repo = g.get_repo(f"{owner}/{repoName}")
-  branchList = list(repo.get_branches())
-  try:
-    repo.get_branch(branchName)
-  except Exception:
-    pass
-    branchExists = False
+for owner in ownerList:
+  for repoName in repoList:
+    branchExists = True
+    repo = g.get_repo(f"{owner}/{repoName}")
+    branchList = list(repo.get_branches())
+    try:
+      repo.get_branch(branchName)
+    except Exception:
+      pass
+      branchExists = False
 
-  if (branchExists):
-    print(branchName + ' exists in '+repoName)
-    stageDescription= branchName+" exists in "+ repoName
-    commitStatus = oopsRepo.get_commit(sha=commitId).create_status(
-      state="pending",
-      description=stageDescription,
-      context="Branch Check-"+repoName)
-  else:
-    print(branchName + ' does not exist in '+repoName)
+    if (branchExists):
+      print(branchName + ' exists in '+owner+'/'+repoName)
+      stageDescription= branchName+" exists in "+owner+'/'+ repoName
+      commitStatus = oopsRepo.get_commit(sha=commitId).create_status(
+        state="pending",
+        description=stageDescription,
+        context="Branch Check-"+owner+'/'+repoName)
+    else:
+      print(branchName + ' does not exist in '+owner+'/'+repoName)
