6ad85f2051bde6321439d3b612b91ffd984f87f7
Clementine Gas <43183478+cmgas@users.noreply.github.com>
2021-07-30 07:38:01 -0600

    Add doxygen documentation for ObsDataVector (#1319)
    * Add documentation in ObsDataVector
    
    * More info for read
    
    * Correction ObsVector and not ObsDataVector
    
    * Add names to the parameters in methods
    
    * Comments from reviewers
    
    Co-authored-by: Anna Shlyaeva <shlyaeva@gmail.com>

--- src/oops/interface/ObsDataVector_head.h
+++ src/oops/interface/ObsDataVector_head.h
@@ -23,6 +23,9 @@ namespace oops {
   template <typename OBS> class ObsVector;
 
 // -----------------------------------------------------------------------------
+/// \brief ObsDataVector is a vector templated on data type, in the observation space
+/// \details
+/// oops currently uses ObsDataVector<int> and ObsDataVector<float>);
 
 template <typename OBS, typename DATATYPE>
 class ObsDataVector : public util::Printable,
@@ -32,30 +35,48 @@ class ObsDataVector : public util::Printable,
  public:
   static const std::string classname() {return "oops::ObsDataVector";}
 
-  ObsDataVector(const ObsSpace<OBS> &, const Variables &, const std::string name = "");
-  explicit ObsDataVector(const ObsDataVector &);
-  explicit ObsDataVector(ObsVector<OBS> &);
+  /// Constructor for specified ObsSpace \p os, with \p variables. If the group \p name is
+  /// specified, the data is read from ObsSpace for specified variables and group.
+  /// Otherwise ObsDataVector is allocated for specified variables and filled with zeros.
+  ObsDataVector(const ObsSpace<OBS> & os, const Variables & vars, const std::string name = "");
+  /// Copy constructor from \p other
+  ObsDataVector(const ObsDataVector & other);
+  /// Constructor from \p other ObsVector. ObsDataVector is created with variables from ObsVector
+  /// and assigned ObsVector values. This is only well defined for numeric DATATYPE.
+  explicit ObsDataVector(ObsVector<OBS> & other);
+  /// Destructor (defined explicitly for timing and tracing)
   ~ObsDataVector();
 
-/// Interfacing
+  /// Accessor to the data
   ObsDataVec_ & obsdatavector() {return *data_;}
+  /// const accessor to the data
   const ObsDataVec_ & obsdatavector() const {return *data_;}
 
+  /// Accessor returning pointer to the data
   std::shared_ptr<ObsDataVec_> obsdatavectorptr() {return data_;}
+  /// const accessor returning pointer to the data
   std::shared_ptr<const ObsDataVec_> obsdatavectorptr() const {return data_;}
 
+  /// Assignment operator
   ObsDataVector & operator = (const ObsDataVector &);
 
+  /// Zero out this ObsDataVector
   void zero();
-  void mask(const ObsDataVector<OBS, int> &);
+  /// Mask values by reading another ObsDataVector \p qc that has the same variables and contains
+  /// the masking information. Elements of *this* corresponding to non-zero elements of \p qc are
+  /// set to missing.
+  void mask(const ObsDataVector<OBS, int> & qc);
+  /// Return the number of observations that aren't set to missing, across all MPI tasks.
   unsigned int nobs() const {return data_->nobs();}
 
-// I/O
-  void read(const std::string &);
-  void save(const std::string &) const;
+  /// Fill ObsDataVector with data with group \p name from the associated ObsSpace
+  void read(const std::string & name);
+  /// Save this ObsDataVector as group \p name in the ObsSpace
+  void save(const std::string & name) const;
 
  private:
   void print(std::ostream &) const;
+  /// Pointer to the ObsDataVector implementation
   std::shared_ptr<ObsDataVec_> data_;
 };
 
