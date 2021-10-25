6ca0b3de61622ffa3cc88dfa00476686a8c09e56
Fabio L R Diniz <45880035+fabiolrdiniz@users.noreply.github.com>
2021-10-25 17:32:41 -0300

    adding parameters (#1493)

--- src/oops/runs/EnsVariance.h
+++ src/oops/runs/EnsVariance.h
@@ -21,14 +21,43 @@
 #include "oops/mpi/mpi.h"
 #include "oops/runs/Application.h"
 #include "oops/util/DateTime.h"
+#include "oops/util/parameters/RequiredParameter.h"
 
 namespace oops {
+
+// -----------------------------------------------------------------------------
+
+/// \brief Top-level options taken by the EnsVariance application.
+template <typename MODEL>
+class EnsVarianceParameters : public ApplicationParameters {
+  OOPS_CONCRETE_PARAMETERS(EnsVarianceParameters, ApplicationParameters)
+
+ public:
+  typedef typename Geometry<MODEL>::Parameters_ GeometryParameters_;
+  typedef typename State<MODEL>::Parameters_ StateParameters_;
+  typedef typename Increment<MODEL>::WriteParameters_ IncrementWriteParameters_;
+
+  /// Geometry parameters.
+  RequiredParameter<GeometryParameters_> resolConfig{"geometry", this};
+
+  /// Background parameters.
+  RequiredParameter<StateParameters_> bkgConfig{"background", this};
+
+  /// Ensemble parameters.
+  RequiredParameter<eckit::LocalConfiguration> ensembleConfig{"ensemble", this};
+
+  /// Output increment parameters.
+  RequiredParameter<IncrementWriteParameters_> outputConfig{"variance output", this};
+};
+
+// -----------------------------------------------------------------------------
 
 template <typename MODEL> class EnsVariance : public Application {
   typedef IncrementEnsemble<MODEL>                 Ensemble_;
   typedef Geometry<MODEL>                          Geometry_;
   typedef Increment<MODEL>                         Increment_;
   typedef State<MODEL>                             State_;
+  typedef EnsVarianceParameters<MODEL>             EnsVarianceParameters_;
 
  public:
   // -----------------------------------------------------------------------------
@@ -37,24 +66,26 @@ template <typename MODEL> class EnsVariance : public Application {
   virtual ~EnsVariance() {}
   // -----------------------------------------------------------------------------
   int execute(const eckit::Configuration & fullConfig) const {
-    // Setup Geometry
-    const eckit::LocalConfiguration resolConfig(fullConfig, "geometry");
-    const Geometry_ resol(resolConfig, this->getComm());
+//  Deserialize parameters
+    EnsVarianceParameters_ params;
+    params.validateAndDeserialize(fullConfig);
 
-    // Setup background
-    const eckit::LocalConfiguration bkgConfig(fullConfig, "background");
-    State_ xx(resol, bkgConfig);
+//  Setup Geometry
+    const Geometry_ resol(params.resolConfig, this->getComm(), oops::mpi::myself());
 
-    // Compute transformed ensemble perturbations
-    //        ens_k = K^-1 dx_k
-    const eckit::LocalConfiguration ensConfig(fullConfig, "ensemble");
+//  Setup background
+    State_ xx(resol, params.bkgConfig);
+
+//  Compute transformed ensemble perturbations
+//         ens_k = K^-1 dx_k
+    const eckit::LocalConfiguration ensConfig = params.ensembleConfig;
     Variables vars(ensConfig, "output variables");
     Ensemble_ ens_k(ensConfig, xx, xx, resol, vars);
 
-    // Get ensemble size
+//  Get ensemble size
     unsigned nm = ens_k.size();
 
-    // Compute ensemble standard deviation
+//  Compute ensemble standard deviation
     Increment_ km1dx(ens_k[0]);
     km1dx.zero();
     Increment_ sigb2(km1dx);
@@ -63,16 +94,15 @@ template <typename MODEL> class EnsVariance : public Application {
     for (unsigned jj = 0; jj < nm; ++jj) {
       km1dx = ens_k[jj];
 
-      // Accumulate km1dx^2
+//    Accumulate km1dx^2
       km1dx.schur_product_with(km1dx);
       sigb2 += km1dx;
     }
     const double rk = 1.0/(static_cast<double>(nm) - 1.0);
     sigb2 *= rk;
 
-    // Write variance to file
-    const eckit::LocalConfiguration varianceout(fullConfig, "variance output");
-    sigb2.write(varianceout);
+//  Write variance to file
+    sigb2.write(params.outputConfig);
     Log::test() << "Variance: " << std::endl << sigb2 << std::endl;
 
     return 0;
