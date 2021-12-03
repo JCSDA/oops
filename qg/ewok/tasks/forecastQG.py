e3dc5fcb36edcf5263ae05af00e260b58354ce5b
Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>
2021-12-03 09:55:05 -0700

    QG specific forecast task example (#1564)
    * QG specific task (and licence for ewok files)
    
    * Comments

---
+++ qg/ewok/tasks/forecastQG.py
@@ -0,0 +1,21 @@
+# (C) Copyright 2020-2021 UCAR
+# 
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+import os
+import yamltools
+import ewok.tasks.forecast as generic
+
+# Specific task inherits from generic but this is not mandatory
+class forecastQG(generic.forecast):
+
+    def setup(self, config, execs, ic):
+        print("Showing off with a specific forecast for QG")
+
+        # Setting generic defaults (but task could set its own)
+        generic.forecast.setup(self, config, execs, ic)
+
+        # Using specific script (generic one could be used as well)
+        self.command = os.path.join(config['model_path'],"tasks/qg-run.sh")
+
---
+++ qg/ewok/tasks/qg.py
@@ -0,0 +1,17 @@
+# (C) Copyright 2020-2021 UCAR
+# 
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+import ewok.tasks.GenericModel
+import forecastQG
+  
+# Inherit from generic tasks so tasks default to generic.
+# It is possible not to inherit if all tasks are defined here.
+class ModelTasks(ewok.tasks.GenericModel.GenericModelTasks):
+
+    def __init__(self):
+        ewok.tasks.GenericModel.GenericModelTasks.__init__(self)
+
+        self.forecast = forecastQG.forecastQG
+
