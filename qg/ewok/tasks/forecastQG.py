3da99b3892ade7b3003954bea8e470c738d714b0
Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>
2021-12-18 20:33:45 -0700

    Support for fix files in ewok (#1577)

--- qg/ewok/tasks/forecastQG.py
+++ qg/ewok/tasks/forecastQG.py
@@ -10,11 +10,11 @@ import ewok.tasks.forecast as generic
 # Specific task inherits from generic but this is not mandatory
 class forecastQG(generic.forecast):
 
-    def setup(self, config, execs, ic):
+    def setup(self, config, execs, fix, ic):
         print("Showing off with a specific forecast for QG")
 
         # Setting generic defaults (but task could set its own)
-        generic.forecast.setup(self, config, execs, ic)
+        generic.forecast.setup(self, config, execs, fix, ic)
 
         # Using specific script (generic one could be used as well)
         self.command = os.path.join(config['model_path'],"tasks/qg-run.sh")
