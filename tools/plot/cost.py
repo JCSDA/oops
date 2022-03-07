a5c742730463fc9ebc70d383e5ff8c5d70ee5aa9
Clementine Gas <43183478+cmgas@users.noreply.github.com>
2022-03-07 11:51:18 -0700

    Plot quadratic cost function in toy-model tools (#1664)
    * First commit
    
    * Plot quad J on non linear J plots
    
    * remove show plot
    
    * different markes
    
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

--- tools/plot/cost.py
+++ tools/plot/cost.py
@@ -13,9 +13,11 @@ import matplotlib.pyplot as plt
 def subfunc(args):
     # Observation types
 
-    # Read log file to find observation types and count outer iterations
+    # Read log file to find observation types and count outer iterations (nouter)
+    # and total inner iterations (ninner)
     obs_types = []
-    niter = 0
+    nouter = 0
+    ninner = 0
     with open(args.filepath, "r") as file:
         for line in file:
             # Get observation types
@@ -25,48 +27,76 @@ def subfunc(args):
                if not obs_type in obs_types:
                   obs_types.append(obs_type)
 
-            # Get nonlinear J
+            # Get number of outer iterations (+1 with initial value)
             pattern = "Nonlinear J ="
             if re.search(pattern, line):
-                niter += 1
+                nouter += 1
+
+            # Get total number of inner iterations
+            pattern = "Quadratic cost function: J "
+            if re.search(pattern, line):
+                ninner += 1
+
+    print(nouter-1, "outer iterations with a total of", ninner, "inner iterations")
 
     # Initialization
-    Jb = np.zeros((niter))
-    Jo = np.zeros((niter, len(obs_types)))
-    J = np.zeros((niter))
+    Jb = np.zeros((nouter))
+    Jo = np.zeros((nouter, len(obs_types)))
+    J = np.zeros((nouter))
+    indexJ = []
+    quadJ = np.zeros((ninner))
+    indexQuadJ = np.zeros((ninner))
 
     # Read log file to get cost function
     with open(args.filepath, "r") as file:
         iter = 0
         for line in file:
             # Get nonlinear Jb
-            pattern = "Nonlinear Jb ="
+            pattern = "Test     : CostJb   : Nonlinear Jb ="
             if re.search(pattern, line):
-                Jb[iter] = float(line.split()[5])
+                Jb[iter] = float(line.split("=")[1])
 
             # Get nonlinear Jo
             for iobs in range(0, len(obs_types)):
-               pattern = "Nonlinear Jo\(" + obs_types[iobs] + "\) ="
+               pattern = "Test     : CostJo   : Nonlinear Jo\(" + obs_types[iobs] + "\) ="
                if re.search(pattern, line):
                   Jo[iter, iobs] = float(line.split("=", 1)[1].split(",", 1)[0])
 
             # Get nonlinear J
-            pattern = "Nonlinear J ="
+            pattern = "Test     : CostFunction: Nonlinear J ="
             if re.search(pattern, line):
-                J[iter] = float(line.split()[4])
+                J[iter] = float(line.split("=")[1])
                 iter += 1
 
+
+    with open(args.filepath, "r") as file:
+        iter = 0
+        for line in file:
+            # Get quadratic J
+            pattern = "Quadratic cost function: J "
+            if re.search(pattern, line):
+                indexQuadJ[iter] = int(re.split("[()]", line)[1])
+                if indexQuadJ[iter] == 1:
+                    indexJ.append(iter)
+                quadJ[iter] = float(line.split("=")[1])
+                iter += 1
+        indexJ.append(iter)
+
     # Post-processing
-    iters = np.linspace(1, niter, niter, dtype=np.int8)
+    iters = np.linspace(1, ninner, ninner, dtype=np.int8)
     Jo_tot = J-Jb
 
     # Plot cost function
     fig, ax = plt.subplots()
-    ax.set_title("Nonlinear cost function")
-    ax.set_xlabel("Outer iteration")
-    ax.set_ylabel("Jb (red), Jo (blue) and J (black)")
+    ax.set_title("Nonlinear and quadratic cost functions")
+    ax.set_xlabel("Inner iteration")
+    ax.set_ylabel("Cost function")
     plt.xticks(iters, iters)
-    ax.plot(iters, Jb, "r-", iters, Jo_tot, "b-", iters, J, "k-")
+    ax.plot(indexJ, Jb, "rv", label='Jb')
+    ax.plot(indexJ, Jo_tot, "bo", label='Jo', fillstyle="none")
+    ax.plot(indexJ, J, "kx", label='J', ms=7)
+    ax.plot(iters, quadJ, "k-", label='quad J')
+    ax.legend()
 
     # Save plot
     if args.output is None:
