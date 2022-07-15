d93f5eaf902519e6b7867a43c311231b8544adc9
Benjamin Menetrier <30638301+benjaminmenetrier@users.noreply.github.com>
2022-07-15 08:40:03 -0600

    NVHPC compiler (#1791)
    * CXX flags
    
    * Fixes
    
    * Switch off l95 simplified tests
    
    * Reset l95 CMakeLists.txt
    
    * O2 instead of O3

--- cmake/compiler_flags_NVHPC_CXX.cmake
+++ cmake/compiler_flags_NVHPC_CXX.cmake
@@ -1,4 +1,4 @@
-# (C) Copyright 2022 UCAR
+# (C) Copyright 2019 UCAR
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -7,41 +7,35 @@
 # FLAGS COMMON TO ALL BUILD TYPES
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Ktrap=fp")
-if( HAVE_OMP )
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -mp")
-else( )
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -nomp")
-endif()
+set( CMAKE_CXX_FLAGS     "${CMAKE_CXX_FLAGS} -Mpreprocess" )
 
 ####################################################################
 # RELEASE FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_RELEASE "-O2 --fast" )
+set( CMAKE_CXX_FLAGS_RELEASE     "-O2 --fast" )
 
 ####################################################################
 # DEBUG FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -Mbounds -Mchkstk -traceback" )
+set( CMAKE_CXX_FLAGS_DEBUG       "-O0 -g -Mbounds -Mchkstk -traceback" )
 
 ####################################################################
 # BIT REPRODUCIBLE FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_BIT     "-O2 --fast" )
+set( CMAKE_CXX_FLAGS_BIT         "-O2 --fast" )
 
 ####################################################################
 # LINK FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_LINK_FLAGS    "" )
+set( CMAKE_CXX_LINK_FLAGS     "" )
 
 ####################################################################
 
 # Meaning of flags
 # ----------------
-# -fstack-arrays     : Allocate automatic arrays on the stack (needs large stacksize!!!)
-# -funroll-all-loops : Unroll all loops
-# -fcheck=bounds     : Bounds checking
+# todo
+
