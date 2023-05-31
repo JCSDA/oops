6287c4d06048ab3089e7ac66dbd7d9195499f3c4
Maryam Abdi <maryamao@ucar.edu>
2023-05-31 11:18:24 -0600

    2D variables in latlon (#2190)
    * save
    
    * typo
    
    * save
    
    * save
    
    * save and cleanup
    
    * cleanup and save
    
    * Get lats/lons directly from atlas grid
    
    * coding norm
    
    ---------
    
    Co-authored-by: Francois Hebert <hebert@ucar.edu>
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

--- src/oops/base/LatLonGridWriter.h
+++ src/oops/base/LatLonGridWriter.h
@@ -40,25 +40,30 @@ namespace oops {
 namespace detail {
 
 // Originally from SABER quench/Fields.cc file (PointCloud case), with minor changes to...
-// - add dimensions for the number of lats/lons in the regulat latlon grid
+// - add dimensions for the number of lats/lons in the regular latlon grid
 // - write a subset of levels for each upper-air field (and only 1 level for surface fields)
+// - write the variable in 3d (lat x lon x alt)
 void writer(const atlas::FunctionSpace & fs,
             const atlas::FieldSet & fset,
             const Variables & vars,
             const std::vector<size_t> & levels,
             const std::string & filepath,
-            const size_t nlats,
-            const size_t nlons) {
+            const std::vector<double> & lats,
+            const std::vector<double> & lons) {
   // Get sizes
-  atlas::idx_t npts = fs.size();
-  atlas::idx_t nz = levels.size();
+  const size_t nlats = lats.size();
+  const size_t nlons = lons.size();
+  const atlas::idx_t npts = fs.size();
+  const atlas::idx_t nz = levels.size();
   ASSERT(nlats * nlons == static_cast<size_t>(npts));
 
   // NetCDF IDs
   int ncid, retval;
-  int npts_id, nlats_id, nlons_id, nz_id, nzsfc_id;
-  int d1D_id[1], d2D_id[2];
-  int levels_id, lon_id, lat_id, var_id[vars.size()];
+  int npts_id, nz_id, nzsfc_id, lat_dimid, lon_dimid;
+  int d1D_id[1], d3D_id[3];
+  int levels_id, var_id[vars.size()];
+  int lat_id, lon_id;
+
 
   // NetCDF file path
   std::string ncfilepath = filepath;
@@ -70,8 +75,8 @@ void writer(const atlas::FunctionSpace & fs,
 
   // Create dimensions
   if ((retval = nc_def_dim(ncid, "npoints", npts, &npts_id))) ERR(retval);
-  if ((retval = nc_def_dim(ncid, "nlats", nlats, &nlats_id))) ERR(retval);
-  if ((retval = nc_def_dim(ncid, "nlons", nlons, &nlons_id))) ERR(retval);
+  if ((retval = nc_def_dim(ncid, "latitude", nlats, &lat_dimid))) ERR(retval);
+  if ((retval = nc_def_dim(ncid, "longitude", nlons, &lon_dimid))) ERR(retval);
   if ((retval = nc_def_dim(ncid, "nlevels", nz, &nz_id))) ERR(retval);
   if ((retval = nc_def_dim(ncid, "nlevels_surface", 1, &nzsfc_id))) ERR(retval);
 
@@ -80,24 +85,35 @@ void writer(const atlas::FunctionSpace & fs,
   if ((retval = nc_def_var(ncid, "model_levels_used", NC_INT, 1, d1D_id, &levels_id))) ERR(retval);
 
   // Define coordinates
-  d1D_id[0] = npts_id;
-  if ((retval = nc_def_var(ncid, "lon", NC_DOUBLE, 1, d1D_id, &lon_id))) ERR(retval);
-  if ((retval = nc_def_var(ncid, "lat", NC_DOUBLE, 1, d1D_id, &lat_id))) ERR(retval);
+  if ((retval = nc_def_var(ncid, "latitude", NC_DOUBLE, 1, &lat_dimid, &lat_id))) ERR(retval);
+  if ((retval = nc_def_var(ncid, "longitude", NC_DOUBLE, 1, &lon_dimid, &lon_id))) ERR(retval);
+
+  // add unit attributes
+  if ((retval = nc_put_att_text(ncid, lat_id, "units",
+    strlen("degrees"), "degrees")))
+    ERR(retval);
+
+  if ((retval = nc_put_att_text(ncid, lon_id, "units",
+    strlen("degrees"), "degrees")))
+    ERR(retval);
 
   // Define variables
-  d2D_id[0] = npts_id;
+  d3D_id[0] = lat_id;
+  d3D_id[1] = lon_id;
+
   for (size_t jvar = 0; jvar < vars.size(); ++jvar) {
     const size_t var_levs = fset.field(vars[jvar]).levels();
     if (var_levs > 1) {
       // Upper-air data case
       // Sanity check we didn't request more levels than the model has
       ASSERT(static_cast<size_t>(nz) <= var_levs);
-      d2D_id[1] = nz_id;
+      d3D_id[2] = nz_id;
     } else {
       // Surface data case
-      d2D_id[1] = nzsfc_id;
+      d3D_id[2] = nzsfc_id;
     }
-    if ((retval = nc_def_var(ncid, vars[jvar].c_str(), NC_DOUBLE, 2, d2D_id, &var_id[jvar]))) {
+
+    if ((retval = nc_def_var(ncid, vars[jvar].c_str(), NC_DOUBLE, 3, d3D_id, &var_id[jvar]))) {
       ERR(retval);
     }
   }
@@ -113,42 +129,31 @@ void writer(const atlas::FunctionSpace & fs,
   // Write levels
   if ((retval = nc_put_var_int(ncid, levels_id, &zlevels[0][0]))) ERR(retval);
 
-  // Copy coordinates
-  double zlon[npts][1];
-  double zlat[npts][1];
-  auto lonlatView = atlas::array::make_view<double, 2>(fs.lonlat());
-  for (atlas::idx_t i = 0; i < npts; ++i) {
-    zlon[i][0] = lonlatView(i, 0);
-    zlat[i][0] = lonlatView(i, 1);
-  }
-
   // Write coordinates
-  if ((retval = nc_put_var_double(ncid, lon_id, &zlon[0][0]))) ERR(retval);
-  if ((retval = nc_put_var_double(ncid, lat_id, &zlat[0][0]))) ERR(retval);
+  if ((retval = nc_put_var_double(ncid, lat_id, lats.data()))) ERR(retval);
+  if ((retval = nc_put_var_double(ncid, lon_id, lons.data()))) ERR(retval);
 
   for (size_t jvar = 0; jvar < vars.size(); ++jvar) {
     const size_t var_levs = fset.field(vars[jvar]).levels();
     auto varView = atlas::array::make_view<double, 2>(fset[vars[jvar]]);
     if (var_levs > 1) {
-      // Upper-air data case
-      // Copy data
-      double zvar[npts][nz];
+      double zvar2d[nlats][nlons][nz];
       for (atlas::idx_t k = 0; k < nz; ++k) {
-        for (atlas::idx_t i = 0; i < npts; ++i) {
-          zvar[i][k] = varView(i, levels[k]);
+        for (size_t j = 0; j < nlats; ++j) {
+          for (size_t i = 0; i < nlons; ++i) {
+            zvar2d[j][i][k] = varView(j*nlons+i, levels[k]);
+          }
         }
       }
-      // Write data
-      if ((retval = nc_put_var_double(ncid, var_id[jvar], &zvar[0][0]))) ERR(retval);
+      if ((retval = nc_put_var_double(ncid, var_id[jvar], &zvar2d[0][0][0]))) ERR(retval);
     } else {
-      // Surface data case
-      // Copy data
-      double zvar[npts][1];
-      for (atlas::idx_t i = 0; i < npts; ++i) {
-        zvar[i][0] = varView(i, 0);
+      double zvar2d[nlats][nlons][nz];
+      for (size_t j = 0; j < nlats; ++j) {
+        for (size_t i = 0; i < nlons; ++i) {
+          zvar2d[j][i][0] = varView(j*nlons+i, 0);
+        }
       }
-      // Write data
-      if ((retval = nc_put_var_double(ncid, var_id[jvar], &zvar[0][0]))) ERR(retval);
+      if ((retval = nc_put_var_double(ncid, var_id[jvar], &zvar2d[0][0][0]))) ERR(retval);
     }
   }
 
@@ -228,10 +233,12 @@ LatLonGridWriter<MODEL>::LatLonGridWriter(
   const auto isNearlyInt = [](const double x) { return fabs(round(x) - x) < 1.e-6; };
   gridRes_ = static_cast<size_t>(isNearlyInt(ratio) ? round(ratio) : ceil(ratio));
 
-  // Want final results on proc0 only, so only proc0 gets points in the PointCloud
+  // Want final results on proc0 only, so only proc0 gets points in the PointCloud.
+  // This requires passing an empty but valid FunctionSpaces to the interpolator, something that we
+  // can only do with PointCloud. (This is a bit hacky and could probably be cleaned up.)
   if (comm_.rank() == 0) {
     const std::string atlasGridName = "L" + std::to_string(gridRes_);
-    const atlas::Grid grid(atlasGridName);
+    const atlas::RegularLonLatGrid grid(atlasGridName);
     targetFunctionSpace_.reset(new atlas::functionspace::PointCloud(grid));
   } else {
     const std::vector<atlas::PointXY> empty{{}};
@@ -266,9 +273,24 @@ void LatLonGridWriter<MODEL>::interpolateAndWrite(const FLDS & xx) const {
   // Write data on proc0 only
   if (comm_.rank() == 0) {
     const std::string filepath = path_ + "/" + prefix_ + "." + xx.validTime().toStringIO();;
-    const size_t nlats = 2*gridRes_ + 1;
-    const size_t nlons = 4*gridRes_;
-    detail::writer(*targetFunctionSpace_, fsetLatLon, vars_, levels_, filepath, nlats, nlons);
+
+    // Because we're using a PointCloud FunctionSpace (for reasons discussed above), we have to
+    // reconstruct the grid here to obtain the lats/lons for writing to file. (The PointCloud is
+    // so generic it doesn't rely on or give access to a grid.)
+    const std::string atlasGridName = "L" + std::to_string(gridRes_);
+    const atlas::RegularLonLatGrid grid(atlasGridName);
+    const size_t nlats = grid.ny();
+    const size_t nlons = grid.nx();
+    std::vector<double> lats(nlats);
+    std::vector<double> lons(nlons);
+    for (size_t i = 0; i < nlats; ++i) {
+      lats[i] = grid.lat(i);
+    }
+    for (size_t i = 0; i < nlons; ++i) {
+      lons[i] = grid.lon(i);
+    }
+
+    detail::writer(*targetFunctionSpace_, fsetLatLon, vars_, levels_, filepath, lats, lons);
   }
 }
 
