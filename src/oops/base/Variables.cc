78434af9a05dfd31c34f21c76f847428231f39b3
Steve Vahl <42982505+svahl991@users.noreply.github.com>
2024-08-05 05:11:30 -0600

    [BUG] Variables push_back not working as intended (#2713)
    * Call Variables::push_back instead of std::vector<string>::push_back
    
    * Remove extra whitespace
    
    ---------
    
    Co-authored-by: Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>

--- src/oops/base/Variables.cc
+++ src/oops/base/Variables.cc
@@ -141,9 +141,8 @@ void Variables::push_back(const Variable & var) {
 // -----------------------------------------------------------------------------
 
 void Variables::push_back(const std::string & var) {
-  vars_.push_back(Variable(var));
+  push_back(Variable(var));
 }
-
 
 // -----------------------------------------------------------------------------
 
