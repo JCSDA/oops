46dc6b1ac5db3c0c7a0a990be587ceec1b3ca276
Chris Thomas <32307951+ctgh@users.noreply.github.com>
2025-03-11 17:38:30 +0000

    Convert Fortran ETKF code to C++ (#2818)
    * Initial draft done but test failed
    
    * Compiled but incremnet not correct
    
    * Correct increment error
    
    * Define RSqrt before computeWeights
    
    * Correct Yb^T to Yb in PertGETKF pert. update
    
    * Add date to random seed
    
    * Fix random seed issue
    
    * Iniatlized nonlinear processoer first in linear getkf
    
    * Add pert getkf as a subclass of getkf
    
    * Merge develop and add pertGETKF test output
    
    * Merge develop and clean up
    
    * Remove temp files
    
    * Perturb full obs (incomplete)
    
    * Add obs perturbation to Yb_
    
    * add obs perturbation to Yb_
    
    * add obs perturbation to Yb_
    
    * Add perturbed letkf and clean up
    
    * Minor clean-up
    
    * Remove YpertDepEns in GETKFSolverPert and minor change
    
    * common ETKF routine in C++
    
    * header and implementation file
    
    * Remove obs tmp object for perturbed obs
    
    * Minor change to match recent inline EnKF merge
    
    * Minor clean up
    
    * modifications following review
    
    * remove LETKF test for now
    
    * Add const and noalias() where needed
    
    * notation
    
    * corrected equations
    
    * simplify use of norm
    
    * improve speed of wa calculation
    
    ---------
    
    Co-authored-by: bhuang95 <huangbo9955@gmail.com>
    Co-authored-by: Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>

--- src/oops/assimilation/GETKFSolver.h
+++ src/oops/assimilation/GETKFSolver.h
@@ -111,6 +111,7 @@ class GETKFSolver : public LocalEnsembleSolver<MODEL, OBS> {
   VerticalLocEV_ vertloc_;
   size_t neig_;
   size_t nanal_;
+  bool fortranETKF_;
 
   DeparturesEnsemble_ HZb_;
 
@@ -127,7 +128,9 @@ GETKFSolver<MODEL, OBS>::GETKFSolver(ObsSpaces_ & obspaces, const Geometry_ & ge
   : LocalEnsembleSolver<MODEL, OBS>(obspaces, geometry, config, nens, xbmean, incvars),
     nens_(nens), geometry_(geometry),
     vertloc_(config.getSubConfiguration("local ensemble DA.vertical localization"), xbmean[0],
-    incvars), neig_(vertloc_.neig()), nanal_(neig_*nens_), HZb_(obspaces, nanal_)
+    incvars), neig_(vertloc_.neig()), nanal_(neig_*nens_),
+    fortranETKF_(config.getBool("local ensemble DA.fortran ETKF", true)),
+    HZb_(obspaces, nanal_)
 {
   // pre-allocate transformation matrices
   Wa_.resize(nanal_, nens);
@@ -320,12 +323,9 @@ void GETKFSolver<MODEL, OBS>::computeWeights(const Eigen::VectorXd & dy,
                                              const Eigen::VectorXd & R_invvar) {
   // compute transformation matrix, save in Wa_, wa_
   // Yb(nobs,neig*nens), YbOrig(nobs,nens)
-  // uses GSI GETKF code
   util::Timer timer(classname(), "computeWeights");
   const LocalEnsembleSolverInflationParameters & inflopt = this->options_.infl;
   const float infl = inflopt.mult;
-
-  const int nobsl = dy.size();
 
   // cast eigen<double> to eigen<float>
   const Eigen::VectorXf dy_f = dy.cast<float>();
@@ -336,14 +336,57 @@ void GETKFSolver<MODEL, OBS>::computeWeights(const Eigen::VectorXd & dy,
   Eigen::MatrixXf Wa_f(nanal_, this->nens_);
   Eigen::VectorXf wa_f(nanal_);
 
-  // call into GSI interface to compute Wa and wa
-  const int getkf_inflation = 0;
-  const int denkf = 0;
-  const int getkf = 1;
-  letkf_core_f90(nobsl, Yb_f.data(), YbOrig_f.data(), dy_f.data(),
-                 wa_f.data(), Wa_f.data(),
-                 R_invvar_f.data(), nanal_, neig_,
-                 getkf_inflation, denkf, getkf, infl);
+  if (fortranETKF_) {
+    // call into GSI interface to compute Wa and wa
+    const int nobsl = dy.size();
+    const int getkf_inflation = 0;
+    const int denkf = 0;
+    const int getkf = 1;
+    letkf_core_f90(nobsl, Yb_f.data(), YbOrig_f.data(), dy_f.data(),
+                   wa_f.data(), Wa_f.data(),
+                   R_invvar_f.data(), nanal_, neig_,
+                   getkf_inflation, denkf, getkf, infl);
+  } else {
+    // Identity
+    const Eigen::VectorXf I = Eigen::VectorXf::Constant(this->nanal_, 1.0);
+
+    // Yb R^-1
+    const Eigen::MatrixXf YbRinv = Yb_f * R_invvar_f.asDiagonal();
+
+    // Yb R^-1 Yb^T + (nens - 1) I / infl
+    const Eigen::MatrixXf YbRinvYbpI =
+      YbRinv * Yb_f.transpose() +
+      I.asDiagonal().toDenseMatrix() * (nens_ - 1) / infl;
+
+    // Eigendecomposition
+    const Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> es(YbRinvYbpI);
+    const Eigen::VectorXf eival = es.eigenvalues().real();
+    const Eigen::MatrixXf eivec = es.eigenvectors().real();
+
+    // Pa = [Yb R^-1 Yb^T + (nens - 1)/infl I]^-1
+    const Eigen::MatrixXf Pa =
+      eivec * eival.cwiseInverse().asDiagonal() * eivec.transpose();
+
+    // wa_f = Pa Yb R^-1 dy
+    wa_f.noalias() = Pa * (YbRinv * dy_f);
+
+    // Normalisation
+    const float norm = 1.0 / (nens_ - 1.0);
+
+    // (I - Gamma^{-1/2} / (nens - 1)) * (Gamma - (nens - 1) I / rho)
+    const Eigen::VectorXf diag = (I - (norm * eival).cwiseInverse().cwiseSqrt()).
+      cwiseProduct((eival - I / (infl * norm)).cwiseInverse());
+
+    // C ((I - Gamma^{-1/2} / (nens - 1)) * (Gamma - (nens - 1) I / rho)) C^T
+    const Eigen::MatrixXf scaledCCT = eivec * diag.asDiagonal() * eivec.transpose();
+
+    // Yb R^-1 YbOrig^T
+    const Eigen::MatrixXf YbRinvYbOrig = YbRinv * YbOrig_f.transpose();
+
+    // Wa_f
+    Wa_f.noalias() = -scaledCCT * YbRinvYbOrig;
+  }
+
   this->Wa_ = Wa_f.cast<double>();
   this->wa_ = wa_f.cast<double>();
 }
