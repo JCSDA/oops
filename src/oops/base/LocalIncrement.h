0a7ae7f5c4d1673a5b08e64de1fa561418cb85d0
Chris Thomas <32307951+ctgh@users.noreply.github.com>
2025-07-22 23:56:54 +0100

    print LocalIncrement with zero entries (#2958)

--- src/oops/base/LocalIncrement.h
+++ src/oops/base/LocalIncrement.h
@@ -32,8 +32,13 @@ class LocalIncrement: public util::Printable {
 
  private:
   void print(std::ostream & os) const {
-    os << "LocalIncrement, size: " << vals_.size() << ", first element: "
-       << vals_[0] << std::endl; }
+    const size_t numValues = vals_.size();
+    os << "LocalIncrement, size: " << numValues;
+    if (numValues > 0) {
+      os << ", first element: " << vals_[0];
+    }
+    os << std::endl;
+  }
   const oops::Variables vars_;      // variables in the object
   std::vector<double> vals_;        // data in flat array
   const std::vector<int> varlens_;  // vector containing nlevs for each variable
