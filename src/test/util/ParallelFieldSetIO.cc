e5d7482622af08a684c15c5969decc3eb21a5793
David Davies <david.davies@metoffice.gov.uk>
2025-11-05 14:37:17 +0000

    Fix for AOCC (#3100)

--- src/test/util/ParallelFieldSetIO.cc
+++ src/test/util/ParallelFieldSetIO.cc
@@ -8,7 +8,9 @@
 
 #include "test/util/ParallelFieldSetIO.h"
 
+#ifndef AOCC
 #include <filesystem>
+#endif
 #include <algorithm>
 
 #include "atlas/functionspace/BlockStructuredColumns.h"
@@ -137,7 +139,11 @@ void test(const size_t configNumber) {
         const auto ncfilepathSerial = TestEnvironment::config().getString("datadir") + "/" +
                                       "test_util_parallelfieldsetio_1PE-" +
                                       "config" + std::to_string(configNumber) + ".nc";
+#ifdef AOCC
+        if (access(ncfilepathSerial.c_str(), 0) == 0) {
+#else
         if (std::filesystem::exists(ncfilepathSerial)) {
+#endif
             parallelFieldSetIO.read(target, ncfilepath);
             EXPECT(util::compareFieldSets(source, target, 1.0e-16));
         } else {
