26dec02a73e84e6e82ba63b104fd99b9a9892118
Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>
2026-01-13 14:58:22 -0700

    No templated inheritance for AnalyticInit (#3097)
    * No templated inheritance for AnalyticInit
    
    * GeoVaLs rms
    
    * Trigger CI

--- l95/src/lorenz95/GomL95.h
+++ l95/src/lorenz95/GomL95.h
@@ -53,13 +53,14 @@ class GomL95 : public util::Printable,
   static const std::string classname() {return "lorenz95::GomL95";}
 
   GomL95(const oops::Locations<L95ObsTraits> & locs,
-         const oops::Variables & vars, const std::vector<size_t> & sizes);
+         const oops::Variables & vars, const std::vector<size_t> & sizes,
+         const eckit::Configuration &);
   GomL95(const eckit::Configuration &, const ObsTable &, const oops::Variables &);
 
   void zero();
   void random();
   double rms() const;
-  double normalizedrms(const GomL95 &) const;
+  double normalizedrms(const GomL95 &, const std::string & dummy = "") const;
   GomL95 & operator*=(const double &);
   GomL95 & operator+=(const GomL95 &);
   GomL95 & operator-=(const GomL95 &);
