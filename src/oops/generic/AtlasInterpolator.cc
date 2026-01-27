5a392ca29ee78f4492b3562f9417e743ec2f76e2
Josh Colclough <109143205+mo-joshuacolclough@users.noreply.github.com>
2026-01-27 14:29:22 +0000

    Copy field metadata in AtlasInterpolator (#3167)
    * Copy metadata correctly
    
    * Update copyright

--- src/oops/generic/AtlasInterpolator.cc
+++ src/oops/generic/AtlasInterpolator.cc
@@ -1,5 +1,5 @@
 
-// (C) Crown Copyright 2022 Met Office
+// (C) Crown Copyright 2022-2026 Met Office
 //
 // This software is licensed under the terms of the Apache Licence Version 2.0
 // which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -276,6 +276,9 @@ atlas::FieldSet AtlasInterpolator::createTargetFields(
     auto targetField = targetFieldSet.add(
         targetFunctionSpace.createField<double>(targetConfig));
     zeroField(targetField);
+
+    // Copy metadata (important for vector fields).
+    targetField.metadata() = sourceField.metadata();
   }
 
   return targetFieldSet;
