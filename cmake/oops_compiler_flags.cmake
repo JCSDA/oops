b7507e3a85ba1a6e93705ff8f18d3a8581ca2ee9
Francois Hebert <hebert@ucar.edu>
2026-03-30 14:21:00 -0600

    Clean up compiler flag specification (#3225)
    * Clean up compiler flag specification
    
    * Update start-jedi-ci.yaml
    
    * trigger oneapi CI
    
    * trigger CI
    
    * Trigger CI
    
    * Trigger CI
    
    ---------
    
    Co-authored-by: Evan Parker <eparker@ucar.edu>
    Co-authored-by: Steve Vahl <42982505+svahl991@users.noreply.github.com>
    Co-authored-by: Yannick Trémolet <tremolet@ucar.edu>

--- cmake/oops_compiler_flags.cmake
+++ cmake/oops_compiler_flags.cmake
@@ -1,71 +1,15 @@
-# (C) Copyright 2009-2016 ECMWF.
-# 
+# (C) Copyright 2026 UCAR
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
-# granted to it by virtue of its status as an intergovernmental organisation nor
-# does it submit to any jurisdiction.
-
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS OFF)
-set(CMAKE_FORTRAN_STANDARD 08)
-set(CMAKE_FORTRAN_STANDARD_REQUIRED ON)
-set(CMAKE_FORTRAN_EXTENSIONS OFF)
-
-if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
-  add_definitions( -DNDEBUG )
-endif( )
-
-#######################################################################################
-# Fortran
-#######################################################################################
-
-if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-  include( compiler_flags_GNU_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-  include( compiler_flags_Intel_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" OR CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC" )
-  include( compiler_flags_NVHPC_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
-  include( compiler_flags_XL_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
-  include( compiler_flags_Cray_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC" )
-  include( compiler_flags_NVHPC_Fortran )
-else()
-  message( STATUS "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
-endif()
-
-#######################################################################################
-# C
-#######################################################################################
-
-# todo
-
-#######################################################################################
-# C++
-#######################################################################################
-
-if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
-  include( compiler_flags_GNU_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM" )
-  include( compiler_flags_Intel_LLVM_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
-  include( compiler_flags_Intel_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "XL" )
-  include( compiler_flags_XL_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "Cray" )
-  include( compiler_flags_Cray_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
-  include( compiler_flags_Clang_CXX )
-elseif( CMAKE_CXX_COMPILER_ID MATCHES "NVHPC" )
-  include( compiler_flags_NVHPC_CXX )
-else()
-  message( STATUS "C++ compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
-endif()
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
 
+# Set compiler flags for basic build types,
+# for compilers where this is not provided by ecbuild.
+include(build_type_compiler_flags)
+
+# Set JEDI's common compiler flags
+include(jedi_common_compiler_flags)
+
+# Set OOPS-specific compiler flags
+# none
